29431e865cd7e376c84c55d91dbd3d6029168a27
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-04-30 03:37:46 +0500

    added MusicListVC

---
+++ Swapping Screens Storyboard/MusicListVC.swift
@@ -0,0 +1,30 @@
+//
+//  MusicListVC.swift
+//  Swapping Screens Storyboard
+//
+//  Created by Muhammad Anjum Kaiser on 30/04/2020.
+//  Copyright © 2020 Muhammad Anjum Kaiser. All rights reserved.
+//
+
+import UIKit
+
+class MusicListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
