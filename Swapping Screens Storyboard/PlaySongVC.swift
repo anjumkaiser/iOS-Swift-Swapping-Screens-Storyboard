2e8cfa0f94a258fb34fc563233c73a280746ac36
Muhammad Anjum Kaiser <anjumbutt@gmail.com>
2020-04-30 12:52:44 +0500

    segue to PlaySongVC with data

--- Swapping Screens Storyboard/PlaySongVC.swift
+++ Swapping Screens Storyboard/PlaySongVC.swift
@@ -1,5 +1,5 @@
 //
-//  MusicListVC.swift
+//  PlaySongVC.swift
 //  Swapping Screens Storyboard
 //
 //  Created by Muhammad Anjum Kaiser on 30/04/2020.
@@ -8,36 +8,37 @@
 
 import UIKit
 
-class MusicListVC: UIViewController {
+class PlaySongVC: UIViewController {
 
+    @IBOutlet weak var songTitleLabel: UILabel!
+    
+    private var _selectedSong: String!
+    
+    var selectedSong: String {
+        get {
+            return _selectedSong
+        }
+        set {
+            _selectedSong = newValue
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        view.backgroundColor = UIColor.blue
-    }
-    @IBAction func backButtonPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        songTitleLabel.text = _selectedSong
     }
     
 
-    @IBAction func loadThirdScreenPressed(_ sender: Any) {
-        
-        let songTitle = "Quit playing games with my heart"
-        performSegue(withIdentifier: "PlaySongVCSegue", sender: songTitle)
-    }
-    
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
-        if let destination = segue.destination as? PlaySongVC {
-            if let song = sender as? String {
-                destination.selectedSong = song
-            }
-        }
     }
+    */
 
 }
